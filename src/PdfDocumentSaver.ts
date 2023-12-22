953affe26071cef17533489e20add18fe0f63cc6
Antoan Angelov <antoan.angelov@gmail.com>
2023-12-22 19:49:54 +0200

    Optimize image embedding by embedding each image at most once

--- src/PdfDocumentSaver.ts
+++ src/PdfDocumentSaver.ts
@@ -1,10 +1,11 @@
 import * as pdfjsLib from 'pdfjs-dist';
-import { degrees, PDFDocument, rgb, StandardFonts, PDFField, PDFFont, PDFImage, PDFForm } from 'pdf-lib';
+import { degrees, PDFDocument, rgb, StandardFonts, PDFField, PDFFont, PDFImage, PDFForm, PDFPage } from 'pdf-lib';
 
 import { PdfDocument } from './PdfDocument';
 import { FormInputValues } from './FormInputValues';
 import { Overlays } from './overlays/Overlays';
 import { ImageType } from './overlays/ImageOverlay';
+import { PageOverlays } from './overlays/PageOverlays';
 
 export class PdfDocumentSaver {
   constructor() {}
@@ -15,40 +16,48 @@ export class PdfDocumentSaver {
     this.populateFormValues(formInputValues, pdfDoc.getForm());
 
     // TODO: validation
-    // TODO: zooming
     // TODO: rotation
-    // TODO: use Bootstrap for styling
     // TODO: refactor
 
-    // Fonts
     const neededFonts: Map<string, PDFFont> = await this.getNeededFonts(overlays, pdfDoc);
-
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    const base64ToPdfImageMap = await this.embedImages(overlays, pdfDoc)
 
     for (const [pageNumber, pageOverlays] of overlays.pagesOverlays) {
         const page = pdfDoc.getPage(pageNumber-1); // in pdflib pages are 0-based
         
-        for (const textOverlay of pageOverlays.textOverlays) {
-            console.log(`antoan saving page ${pageNumber}, y is ${textOverlay.transform.y}`)
-            page.drawText(textOverlay.text, {
-                x: textOverlay.transform.x,
-                y: textOverlay.transform.y,
-                size: textOverlay.textSize,
-                font: neededFonts.get(textOverlay.fontFamily) || helveticaFont,
-                color: rgb(
-                    textOverlay.textColor.red, 
-                    textOverlay.textColor.green,
-                    textOverlay.textColor.blue),
-            })
-        }
+        this.doTextDrawing(pageOverlays, page, neededFonts, helveticaFont);
+        this.doImageDrawing(pageOverlays, pdfDoc, page, base64ToPdfImageMap);
+    }
 
+    return pdfDoc.save()
+  }
+
+  private async embedImages(overlays: Overlays, pdfDoc: PDFDocument): Promise<Map<string, PDFImage>> {
+    const base64ToPdfImageMap: Map<string, PDFImage> = new Map();
+    for (const [pageNumber, pageOverlays] of overlays.pagesOverlays) {
         for (const imageOverlay of pageOverlays.imageOverlays) {
-            const pdfImage = 
-                imageOverlay.imageType == ImageType.PNG ? await pdfDoc.embedPng(imageOverlay.base64)
-                : imageOverlay.imageType == ImageType.JPEG ? await pdfDoc.embedJpg(imageOverlay.base64) 
-                : null;
+            if (base64ToPdfImageMap.has(imageOverlay.base64)) {
+                continue;
+            }
+            const pdfImage = imageOverlay.imageType == ImageType.PNG ? await pdfDoc.embedPng(imageOverlay.base64)
+                : imageOverlay.imageType == ImageType.JPEG ? await pdfDoc.embedJpg(imageOverlay.base64)
+                    : null;
             if (pdfImage == null) {
-                console.log(`Error reading pdfPage - invalid image format. ${imageOverlay}`)
+                console.log(`Error embedding images in PDF - invalid image format. ${imageOverlay}`);
+                continue;
+            }
+            base64ToPdfImageMap.set(imageOverlay.base64, pdfImage);
+        }
+    }
+    return base64ToPdfImageMap;
+  }
+
+    private async doImageDrawing(pageOverlays: PageOverlays, pdfDoc: PDFDocument, page: PDFPage, base64ToPdfImageMap: Map<string, PDFImage>) {
+        for (const imageOverlay of pageOverlays.imageOverlays) {
+            const pdfImage = base64ToPdfImageMap.get(imageOverlay.base64);
+            if (pdfImage == null) {
+                console.log(`Error reading pdfPage - missing mapping for base64. ${imageOverlay}`);
                 continue;
             }
             page.drawImage(pdfImage, {
@@ -58,55 +67,83 @@ export class PdfDocumentSaver {
                 height: imageOverlay.height,
                 rotate: degrees(imageOverlay.transform.rotation),
                 opacity: 1,
-              })
+            });
         }
     }
 
-    return pdfDoc.save()
-  }
+    private doTextDrawing(pageOverlays: PageOverlays, page: PDFPage, neededFonts: Map<string, PDFFont>, helveticaFont: PDFFont) {
+        for (const textOverlay of pageOverlays.textOverlays) {
+            page.drawText(textOverlay.text, {
+                x: textOverlay.transform.x,
+                y: textOverlay.transform.y,
+                size: textOverlay.textSize,
+                font: neededFonts.get(textOverlay.fontFamily) || helveticaFont,
+                color: rgb(
+                    textOverlay.textColor.red,
+                    textOverlay.textColor.green,
+                    textOverlay.textColor.blue),
+            });
+        }
+    }
 
     private populateFormValues(formInputValues: FormInputValues, form: PDFForm) {
         for (const [key, value] of formInputValues.textNameToValue) {
-            form.getTextField(key).setText(value);
+            try {
+                form.getTextField(key).setText(value);
+            } catch(error) {
+                console.log(`Error thrown while updating the text field value of ${key} with ${value}.`, error)
+            }
         }
 
         for (const [key, value] of formInputValues.checkboxNameToValue) {
-            if (value) {
-                form.getCheckBox(key).check();
-            } else {
-                form.getCheckBox(key).uncheck();
+            try {
+                if (value) {
+                    form.getCheckBox(key).check();
+                } else {
+                    form.getCheckBox(key).uncheck();
+                }
+            } catch(error) {
+                console.log(`Error thrown while updating the checkbox value of ${key} with ${value}.`, error)
             }
         }
 
         for (const [key, value] of formInputValues.dropdownNameToSelectedIndex) {
-            const dropdown = form.getDropdown(key);
-            const options = dropdown.getOptions();
-            if (value < 0 || value >= options.length) {
-                continue;
+            try {
+                const dropdown = form.getDropdown(key);
+                const options = dropdown.getOptions();
+                if (value < 0 || value >= options.length) {
+                    continue;
+                }
+                dropdown.select(options[value]);
+            } catch(error) {
+                console.log(`Error thrown while updating the dropdown value of ${key} with ${value}.`, error)
             }
-            dropdown.select(options[value]);
         }
         
         for (const [key, value] of formInputValues.optionNameToSelectedIndex) {
-            const optionsList = form.getOptionList(key);
-            const options = optionsList.getOptions();
-            if (value < 0 || value >= options.length) {
-                continue;
+            try {
+                const optionsList = form.getOptionList(key);
+                const options = optionsList.getOptions();
+                if (value < 0 || value >= options.length) {
+                    continue;
+                }
+                optionsList.select(options[value]);
+            } catch(error) {
+                console.log(`Error thrown while updating the options list value of ${key} with ${value}.`, error)
             }
-            console.log("antoan option selected = " + options[value] + " " + value)
-            optionsList.select(options[value]);
-            console.log("antoan option selected getSelected() = " + optionsList.getSelected())
-
         }
 
         for (const [key, value] of formInputValues.radioGroupNameToSelectedIndex) {
-            const radioGroup = form.getRadioGroup(key);
-            const options = radioGroup.getOptions();
-            console.log("antoan radio options are " + options);
-            if (value < 0 || value >= options.length) {
-                continue;
+            try {
+                const radioGroup = form.getRadioGroup(key);
+                const options = radioGroup.getOptions();
+                if (value < 0 || value >= options.length) {
+                    continue;
+                }
+                radioGroup.select(options[value]);
+            } catch(error) {
+                console.log(`Error thrown while updating the radio group value of ${key} with ${value}.`, error)
             }
-            radioGroup.select(options[value]);
         }
     }
 
